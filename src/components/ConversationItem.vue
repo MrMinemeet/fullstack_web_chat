de07e5c9137b7089c89a7822f543073c86a8cbd2
MrMinemeet <alex@wtf-my-code.works>
2024-03-30 09:05:33 +0100

    Add Some basic elements for chat
    I never used VueJS before, likely not the way it should be used.

---
+++ src/components/ConversationItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+	sender: string
+	message: string
+}>()
+</script>
+
+<template>
+	<div class="chat-entry">
+		<h3>{{ sender }}:</h3>
+		<em>{{ message }}</em>
+	</div>
+</template>
