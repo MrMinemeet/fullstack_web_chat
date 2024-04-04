f8ff8157a712d0937f7959c2622039d6e55d00d8
MrMinemeet <alex@wtf-my-code.works>
2024-04-04 08:30:19 +0200

    Add additional pages using vue-router
    Added page placeholder for Authentication, Profile and moved chatbox from App.vue to HomeView.vue

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,29 @@
-<script setup lang="ts">
-import ChatBox from './components/ChatBox.vue'
-
-
-const msgs: [string, string][] = [
-  ['Alice', 'Hello, Bob!'],
-  ['Bob', 'Hi, Alice!'],
-  ['Alice', 'How are you?'],
-  ['Bob', 'I am good, thanks!'],
-]
-
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="https://upload.wikimedia.org/wikipedia/commons/c/c3/JKU_Logo.svg" width="150" />
-    <h1>FullStack WebChat</h1>
-    powered by shitty code
+	<header>
+	  <img alt="Vue logo" class="logo" src="https://upload.wikimedia.org/wikipedia/commons/c/c3/JKU_Logo.svg" width="150" />
+	  <h1>FullStack WebChat</h1>
+	  powered by shitty code
+    <nav>
+      <RouterLink to="/">Go to Home</RouterLink>
+      <RouterLink to="/profile">Go to Profile</RouterLink>
+    </nav>
   </header>
-
   <main>
-    <div class="wrapper">
-      <ChatBox recipiant="Alice" :conversation="msgs" />
-    </div>
+    <RouterView />
   </main>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
+  background-color: --primary-color;
+  padding: 20px;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+nav ul {
+  list-style-type: none;
+  padding: 0;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+nav ul li {
+  display: inline;
+  margin-right: 10px;
 }
 </style>
