b0bd3c345996d794e330d682cfb817cc913510df
MrMinemeet <alex@wtf-my-code.works>
2024-04-04 09:27:49 +0200

    Move User Image to extra component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,48 @@
+<script setup lang="ts">
+import UserPicture from './components/UserPicture.vue';
+</script>
+
 <template>
-	<header>
-	  <img alt="Vue logo" class="logo" src="https://upload.wikimedia.org/wikipedia/commons/c/c3/JKU_Logo.svg" width="150" />
-	  <h1>FullStack WebChat</h1>
-	  powered by shitty code
-    <nav>
-      <RouterLink to="/">Go to Home</RouterLink>
-      <RouterLink to="/profile">Go to Profile</RouterLink>
+  <header class="header">
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="https://upload.wikimedia.org/wikipedia/commons/c/c3/JKU_Logo.svg"
+      width="150"
+    />
+    <div class="title">
+      <h1>FullStack WebChat</h1>
+      powered by shitty code
+    </div>
+    <nav class="navigation">
+      <RouterLink to="/">Go to Home 🏠</RouterLink>
+      <RouterLink to="/profile"><UserPicture imgSrc="https://uxwing.com/wp-content/themes/uxwing/download/emoji-emoticon/grinning-face-with-big-eyes-emoji-icon.svg"/></RouterLink>
     </nav>
   </header>
+
   <main>
     <RouterView />
   </main>
 </template>
 
 <style scoped>
-header {
-  background-color: --primary-color;
-  padding: 20px;
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  color: --color-text;
+  background-color: #1a4446;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.logo {
+  padding: 10px;
+}
+.navigation {
+  padding: 10px;
+  display: flex;
+  gap: 10px;
 }
 </style>
