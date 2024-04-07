f6578c6c98791c32eec7ac67295661fed8c55421
MrMinemeet <alex@wtf-my-code.works>
2024-04-07 11:29:15 +0200

    Add proper chat list
    It is shown at the left side of the chatbox.

--- frontend/src/components/ChatList.vue
+++ frontend/src/components/ChatList.vue
@@ -1,1 +1,46 @@
 <!-- Holds a box that shows all "recent" chats -->
+<script setup lang="ts">
+import { ref } from 'vue';
+import ChatListItem from './ChatListItem.vue';
+
+const chats = ref([
+  {
+	name: 'Alice',
+	lastMessage: 'Hello, Bob!'
+  },
+  {
+	name: 'John',
+	lastMessage: 'Awesome Job!'
+  },
+  {
+	name: 'a',
+	lastMessage: 'Awesome Job!'
+  },
+  {
+	name: 'b',
+	lastMessage: 'Awesome Job!'
+  },
+  {
+	name: 'c',
+	lastMessage: 'Awesome Job!'
+  },
+]);
+</script>
+
+<template>
+  <div class="chat-list">
+	<div class="chat-list-header">
+	  <h2>Recent Chats</h2>
+	</div>
+	<div class="chat-list-entries">
+	  <ChatListItem v-for="chat in chats" :key="chat.name" :name="chat.name" :lastMessage="chat.lastMessage" />
+	</div>
+  </div>
+</template>
+
+<style scoped>
+.chat-list-entries {
+	overflow-y: auto;
+	max-height: 200px;
+}
+</style>
