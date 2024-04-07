bd2ec1142fbe699b77509ebebdcf5dc2799c703c
MrMinemeet <alex@wtf-my-code.works>
2024-04-07 11:29:15 +0200

    Add proper chat list
    It is shown at the left side of the chatbox.

---
+++ frontend/src/components/ChatListItem.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import UserPicture from './UserPicture.vue'
+defineProps<{
+	name: string
+	lastMessage: string
+}>()
+</script>
+
+<template>
+	<div class="chat-list-entry"> <!-- TODO: Load chat when clicked --> 
+		<UserPicture imgSrc="https://uxwing.com/wp-content/themes/uxwing/download/emoji-emoticon/emoji-tongue-icon.svg" /> <!-- TODO: Get Avatar from DB using name -->
+		<div class="chat-info">
+			<h3>{{ name }}:</h3>
+			<p>{{ lastMessage }}</p>
+		</div>
+  	</div>
+</template>
+
+<style scoped>
+.chat-list-entry {
+	display: flex;
+	gap: 10px;
+	padding: 10px;
+	margin-right: 50px;
+}
+.chat-info {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+}
+.chat-info p {
+	min-width: 100px;
+}
+</style>
--- frontend/src/pages/HomeView.vue
+++ frontend/src/pages/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import ChatBox from '../components/ChatBox.vue'
+import ChatList from '../components/ChatList.vue'
 
 const msgs: [string, string][] = [
   ['Alice', 'Hello, Bob!'],
@@ -10,5 +11,19 @@ const msgs: [string, string][] = [
 </script>
 
 <template>
-  <ChatBox recipiant="Alice" :conversation="msgs" />
+  <div class="home-view">
+    <ChatList class="border-right"/>
+    <ChatBox recipiant="Alice" :conversation="msgs" /> 
+  </div>
 </template>
+
+<style scoped>
+.home-view {
+  display: flex;
+  gap: 10px;
+  padding: 10px;
+}
+.border-right {
+  border-right: 1px solid var(--color-border);
+}
+</style>
