25c0784b7e97651c580ff7f39cd955a7d4e03e01
Bernhard Schertler <bernhard.schertler@pm.me>
2024-04-08 10:26:20 +0200

    Remove broken unused code

---
+++ frontend/src/components/UserProfile.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+defineProps<{
+	profilePicture: string,
+    username: string,
+    status: string
+}>()
+
+</script>
+
+<template>
+	TODO
+</template>
+
+<style scoped>
+</style>
