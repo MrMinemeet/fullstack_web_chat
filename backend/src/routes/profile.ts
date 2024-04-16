19c256648d303480f095b8b9bd2506a5e72e7188
MrMinemeet <alex@wtf-my-code.works>
2024-04-16 17:41:43 +0200

    WIP image upload

--- backend/src/routes/profile.ts
+++ backend/src/routes/profile.ts
@@ -4,7 +4,31 @@ import { dbConn } from '../utils';
 let router = express.Router();
 
 router.post('/uploadPicture', async function(req: Request, res: Response, next: NextFunction) {
+  // Get the username and picture(blob) from the request
+  const { username, pictureB64} = req.body;
 
+  if (!username || !pictureB64) {
+    res.status(400).json({ message: "Invalid body" });
+    return;
+  }
+
+  // TODO: Use JWT to verify the user (users should only be able to upload their own picture)
+
+
+  // Decode the base64 picture
+  const picture = Buffer.from(pictureB64, 'base64');
+
+  const sql = `UPDATE users SET profilePic = ? WHERE username = ?`;
+
+  dbConn.run(sql, [picture, username], (err: Error) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Error uploading picture' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Picture uploaded successfully' });
+  });
 });
 router.get('/retrievePicture', async function(req: Request, res: Response, next: NextFunction) {
   // Get the username from the request
